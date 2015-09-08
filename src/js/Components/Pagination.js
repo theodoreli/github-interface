2d7895344dcbc7427255448a688c5083f7bc6ebf
Theodore <theo@gmail.com>
2015-09-07 18:01:56 -0700

    sweet jspm scss plugin

--- src/js/Components/Pagination.js
+++ src/js/Components/Pagination.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Router, Route, Link } from 'react-router';
 import Actions from '../Actions/Actions';
 import Store from '../Stores/stores';
+import './Pagination.scss!';
 
 export default class Pagination extends React.Component {
   constructor(props) {
@@ -18,14 +19,14 @@ export default class Pagination extends React.Component {
     var number = function(item) {
       console.log(item);
       return (
-        <div key={item.id} onClick={this._pageToGo.bind(this, item)}>
+        <div key={item.id} className="pag-item" onClick={this._pageToGo.bind(this, item)}>
           {item}
         </div>
       )
     }.bind(this);
 
     return (
-      <div>
+      <div clasName="pag-wrapper">
         {Array.from(Array(10).keys()).map(number)}
       </div>
     )
