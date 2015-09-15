524232d0088c5b81c3076098f2baf085fe6e9517
Theodore <theol88@gmail.com>
2015-09-14 23:25:41 -0700

    issue styling and further templating

--- src/js/Components/Issue.js
+++ src/js/Components/Issue.js
@@ -1,15 +1,33 @@
 import React from 'react';
+import reqwest from 'reqwest';
 import './Issue.scss!';
 
 export default class Issue extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
+    this.state = {
+      comments: []
+    };
+  }
+
+  componentDidMount() {
+    reqwest({url: `https://api.github.com/repos/npm/npm/issues/${this.props.location.query.number}/comments`})
+      .then( (res) => {
+        console.log(res);
+        this.setState({comments: res});
+      });
   }
 
 
   render() {
     console.log(this.props)
+    console.log(this.state.comments)
     var issue = this.props.location.query;
+    var formattedBody = function(body) {
+      return body.replace(new RegExp(String.fromCharCode(13), 'g'), <br/>)
+    };
+    console.log(formattedBody(issue.body))
+
     return (
       <div className="issue-wrapper">
         <div className="title">
@@ -22,11 +40,21 @@ export default class Issue extends React.Component {
           </span>
           <span>
             <span className="issue-user-login">{issue.user.login} </span>
-            opened this issue · comments  
+            opened this issue · {this.state.comments.length} comments  
           </span>
 
           <div className="comments-wrapper">
-          </div>
+            <img src={issue.user.avatar_url} />
+            <div className="issue-meat">
+              <div className="issue-meat-meta">
+                <span style={{fontWeight: 400}}>{issue.user.login}</span> commented
+              </div>
+              <div className="issue-meat-body">
+                {issue.body}
+              </div>
+            </div>
+
+          </div> {/* end: comments-wrapper */ }
         </div>
       </div>
     )
