ee714a7e329a963495ca1744f419c5478d4dffa2
Theodore <theol88@gmail.com>
2016-10-31 16:58:52 -0700

    Issue.js cleaned up

--- src/js/Components/Issue.js
+++ src/js/Components/Issue.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import Label from './Label';
 import reqwest from 'reqwest';
-import './Issue.scss!';
 import marked from 'marked';
+
+import Label from './Label';
+import './Issue.scss!';
 
 export default class Issue extends React.Component {
   constructor(props) {
@@ -15,47 +16,47 @@ export default class Issue extends React.Component {
   }
 
   componentDidMount() {
-    console.log(this.props)
-    const doES7Async = async function() {
-        let issueNum = this.props.params.number;
-        let vals = await Promise.all([
-            reqwest( {url: `https://api.github.com/repos/npm/npm/issues/${issueNum}`}),
-            reqwest( {url: `https://api.github.com/repos/npm/npm/issues/${issueNum}/comments`}) 
-        ]);
-        // hitting issues gives us an obj, comments gives us an array
-        this.setState({ 
-            issue: vals[0],
-            comments: vals[1],
-            loaded: true // until this is flipped to true, we dont render the good stuff yet
+    let issueNum = this.props.params.number;
+    let vals = (async function() {
+      return await Promise.all([
+        reqwest({
+          url: `https://api.github.com/repos/npm/npm/issues/${issueNum}`
+        }),
+        reqwest({
+          url: `https://api.github.com/repos/npm/npm/issues/${issueNum}/comments`
         })
-        //vals.forEach(console.log.bind(console)); // this is super cool btw.
-     }.bind(this) // binding to a function like this, it needs be a function expression (having var in the front)
+      ]); // return
+    })();
 
-     doES7Async();
+    this.setState({
+      issue: vals[0],
+      comments: vals[1],
+      loaded: true // until this is flipped to true, we dont render the good stuff yet
+    });
   }
 
 
   render() {
-    //var issue = this.state.issue || { user: {avatar_url: '', login: ''}, body: ''};
     let issue = this.state.issue;
     const markDown = src => {
-        return marked(src, {sanitize: true})
+      return marked(src, {sanitize: true})
     };
     const userLinking = src => {
-        // http://stackoverflow.com/questions/1234712/javascript-replace-with-reference-to-matched-group
-        // http://es5.github.io/#x15.5.4.11 // $& is the matched substring, but we wont use it here unfortunately. nonetheless, cool trick.
-        return src.replace(/@\w+/g, (a,b) => {
-          let trimmed = a.slice(1);
-          return '<a href="//github.com/' + trimmed + '" target="_new">' + a + '</a>'
-        })
+      // http://stackoverflow.com/questions/1234712/javascript-replace-with-reference-to-matched-group
+      // http://es5.github.io/#x15.5.4.11 // $& is the matched substring, but we wont use it here unfortunately. nonetheless, cool trick.
+      return src.replace(/@\w+/g, (a,b) => {
+        let trimmed = a.slice(1);
+        return '<a href="//github.com/' + trimmed + '" target="_new">' + a + '</a>'
+      })
     };
-    const formattedBody = function(src) {
-        return userLinking( markDown(src) );
+    const formattedBody = src => {
+      return userLinking(markDown(src));
     };
 
-    // helper. this function is called for each of an array's elements
-    const convoBuilder = function(issue) {
-      console.log(issue)
+    /*
+     * Mapping function for each element of comments section.
+     */
+    const commentsMapper = issue => {
       return (
         <div key={issue.id}>
           <img src={issue.user.avatar_url} />
@@ -70,8 +71,10 @@ export default class Issue extends React.Component {
       )
     };
 
-    // helper. show this when ajax queries have loaded.
-    const main = (issue) => {
+    /*
+    * Helper function. Only shows when ajax has completed.
+    */
+    const main = issue => {
       return (
         <div>
           <div className="title">
@@ -80,32 +83,30 @@ export default class Issue extends React.Component {
           </div>
           <div className="sub-title">
             <span className={"status " + issue.state }>
-              {issue.state} 
+              {issue.state}
             </span>
             <span>
               <span className="issue-user-login">{issue.user.login} </span>
-              opened this issue · {this.state.comments.length} comments  
+              opened this issue · {this.state.comments.length} comments
               <Label labels={issue.labels} />
             </span>
 
             <div className="comments-wrapper">
-              { [issue].concat(this.state.comments).map(convoBuilder) }
+              {[issue].concat(this.state.comments).map(commentsMapper)}
             </div>
           </div>
         </div>
       )
-    } 
+    }
 
     //==//==
     //==//==
     //==//==
     // THIS IS REACT CLASSES's render()'s RETURN. THE OUTTER GRAND DADDY
     return (
-
       <div className="issue-wrapper">
         {this.state.loaded ? main(this.state.issue) : <div>loading...</div>}
       </div>
-
     )
   }
 }
