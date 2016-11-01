9b4c83a94b56ccdea744244e7c0f66f0d4b4b541
Theodore <theol88@gmail.com>
2016-11-01 11:18:22 -0700

    stores cleanup

--- src/js/Stores/stores.js
+++ src/js/Stores/stores.js
@@ -1,9 +1,10 @@
 import Alt from '../alt'; // remember this is our own alt file, not the contributed module
 import { createStore, datasource } from 'alt/utils/decorators';
-import Actions from '../Actions/Actions';
 import reqwest from 'reqwest';
 import es6Promise from 'es6-promise';
 import parseLinkHeader from 'thlorenz/parse-link-header';
+
+import Actions from '../Actions/Actions';
 
 es6Promise.polyfill();
 
@@ -12,22 +13,10 @@ var req;
 
 const SearchSource = {
   performSearch: {
-    // remotely fetch something (required)
     remote(state) { // this is our Store state
       req = reqwest({url: `https://api.github.com/repos/npm/npm/issues?page=${state.toGetPage}&per_page=25`});
       return req
     },
-
-    // this function checks in our local cache first
-    // if the value is present it'll use that instead (optional).
-/*
-    local(state) {
-      return state.results[state.value] ? state.results : null;
-    },
-*/
-
-    // here we setup some actions to handle our response
-    //loading: SearchActions.loadingResults, // (optional)
 
     success: Actions.ajaxSucc, // (required)
     error: Actions.ajaxFail, // (required)
@@ -40,26 +29,29 @@ const SearchSource = {
   }
 };
 
-// In the example they use these decorators. leave it out, screws up with store emitting events?
+/*
+ * In the example they use these decorators. Leave it out, as it possibly
+ * screws up with store emitting events?
+ */
 //@createStore(Alt)
 //@datasource(SearchSource)
 class StorePage {
     constructor() {
         this.toGetPage = 1;
-        this.pageContents = {}; 
+        this.pageContents = {};
         this.pageLink = {
           first: 1,
           last: 1
         };
 
         this.registerAsync(SearchSource);
-        
+
         this.bindAction(Actions.getPage, this.onSearch);
         this.bindAction(Actions.ajaxSucc, this.onAjaxSucc);
     }
 
+
     onAjaxSucc(data) {
-      // const enforces that we point to the same place in memory. we can alter what it points to, but not the pointer itself.
       const safeGetPage = (placement, links) => {
         if (! (placement in links) ) {return null}
         if (! ('page' in links[placement]) ) return null
@@ -70,11 +62,8 @@ class StorePage {
         return pageNum
       }
 
-      console.log('hey ajax succ')
-      console.log(data)
       // https://github.com/ded/reqwest/issues/134
       var parsed = parseLinkHeader(req.request.getResponseHeader('Link'));
-      console.log(parsed);
 
       this.pageLink = {
         first: safeGetPage('first', parsed),
@@ -86,12 +75,11 @@ class StorePage {
 
     }
 
+
     onSearch(params) {
-        console.log(params)
         this.toGetPage = params.toGetPage;
 
         if (!this.getInstance().isLoading()) {
-          console.log('inside !this.getInstance()');
             this.getInstance().performSearch();
         }
     }
